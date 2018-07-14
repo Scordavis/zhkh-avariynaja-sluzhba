82c7288b7e3dd9443abd8fc2f2a789aa17767c68
Scordavis <scordavis@gmail.com>
2018-07-14 14:31:05 +0300

    header and form

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,4 @@
 $(function() {
 
-	// Custom JS
-
-  // open reports
-$( '.app-content__disclose' ).click(function() {
-  $( this ).parent().find( '.app-content__wrapper' ).toggle( 'fast' );
-});
 
 });
